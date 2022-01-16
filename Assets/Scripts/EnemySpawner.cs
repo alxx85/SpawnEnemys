55a3e1d840b5e079558df25f8bfc6273b4a6eae9
alxx85 <alxx85@mail.ru>
2022-01-16 12:32:04 +0700

    поправлены названия

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private EnemyMover _enemyPrefab;
+    [SerializeField] private EnemyMover _prefab;
 
     private List<SpawnPoint> _spawnPoints;
-    private WaitForSeconds _delayEnemySpawn = new WaitForSeconds(2f);
+    private WaitForSeconds _delay = new WaitForSeconds(2f);
     private bool _isPlaying = true;
 
     private void Start()
     {
         _spawnPoints = new List<SpawnPoint>(GetComponentsInChildren<SpawnPoint>());
-        StartCoroutine(SpawnsEnemy());
+        StartCoroutine(Spawn());
     }
 
-    private IEnumerator SpawnsEnemy()
+    private IEnumerator Spawn()
     {
         while (_isPlaying)
         {
             foreach (SpawnPoint point in _spawnPoints)
             {
-                EnemyMover newEnemy = Instantiate(_enemyPrefab, point.transform.position, Quaternion.identity, point.transform);
+                EnemyMover newEnemy = Instantiate(_prefab, point.transform.position, Quaternion.identity, point.transform);
                 newEnemy.SetEndPoint(point.EndPosition);
                 newEnemy.StartEnemyMove();
-                yield return _delayEnemySpawn;
+                yield return _delay;
             }
         }
     }
